d935723052506ec0a7acd8f480eecd024c3a3c6a
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-04-29 21:48:56 -0500

    se agrega un icono de experiencia laboral

---
+++ src/app/components/icons/BriefcaseIcon.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-briefcase-icon',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  template: `
+      <svg
+        class="{{class}}"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        ><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path
+          d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"
+        ></path><path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2"></path><path
+          d="M12 12l0 .01"></path><path d="M3 13a20 20 0 0 0 18 0"></path>
+          </svg>
+      `,
+})
+export class BriefcaseIconComponent {
+
+  @Input() id: String = '';
+  @Input() class: String = '';
+
+}
